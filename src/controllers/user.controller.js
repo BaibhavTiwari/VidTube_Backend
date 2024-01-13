66c51d58ee26ab7c37af1d846303431c4767a478
Baibhav <baibhavtiwari37@gmail.com>
2024-01-13 14:10:18 +0530

    controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,15 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiError } from "../utils/apiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "OKAY"
-  })
+  const { fullname, email, username, password } = req.body
+  console.log("email :", email);
+  if (
+    [fullname, email, password, username].some((field) =>
+      field?.trim() === "")
+  ) {
+    throw new apiError(400, "All fields are compulsory..")
+  }
 
 })
 export { registerUser }
--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
     console.log(`\n MongoDb connected.. DB HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
-    console.log("MONGODB connection Failed", error);
+    console.log("MONGODB connection Failed~", error);
     process.exit(1);
   }
 }
