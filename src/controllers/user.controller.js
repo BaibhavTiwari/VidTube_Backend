93b670581ea51d3316585e778295d5801c1244ae
Baibhav <baibhavtiwari37@gmail.com>
2024-01-27 19:35:11 +0530

    Models(comment,like,playlist,tweet)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -127,7 +127,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
+      $unsset: {
         refreshToken: undefined
       }
     },
