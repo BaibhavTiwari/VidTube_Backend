07f660074ef58f80d1b8a6a836008d30e33d4c8b
Baibhav <baibhavtiwari37@gmail.com>
2024-01-30 22:16:05 +0530

    controllers

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,12 +1,34 @@
+
+import mongoose, { isValidObjectId } from "mongoose"
+import { like } from "../models/like.model.js"
 import { apiError } from "../utils/apiError.js"
 import { apiResponse } from "../utils/apiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
+const toggleVideoLike = asyncHandler(async (req, res) => {
+  const { videoId } = req.params
 
-const healthcheck = asyncHandler(async (req, res) => {
+})
+
+const toggleCommentLike = asyncHandler(async (req, res) => {
+  const { commentId } = req.params
+
+
+})
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params
+
+}
+)
+
+const getLikedVideos = asyncHandler(async (req, res) => {
 
 })
 
 export {
-  healthcheck
+  toggleCommentLike,
+  toggleTweetLike,
+  toggleVideoLike,
+  getLikedVideos
 }
