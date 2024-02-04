0ea64628c949cd7baf6be28a62c04ffaf124b097
Baibhav <baibhavtiwari37@gmail.com>
2024-02-04 11:09:47 +0530

    dashboard and healthcheck controller ✅

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,11 +8,84 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
 
+  const userId = req.user?._id
+
+  try {
+    const channelStat = await Video.aggregate([
+      {
+        $match: {
+          owner: new mongoose.Types.ObjectId(userId)
+        }
+      },
+      {
+        $lookup: {
+          from: "likes",
+          localField: "_id",
+          foreignField: "video",
+          as: "Likes"
+        }
+      },
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "owner",
+          foreignField: "channel",
+          as: "Subscribers"
+        }
+      },
+      {
+        $group: {
+          _id: null,
+          TotalVideos: { $sum: 1 },
+          TotalViews: { $sum: "$views" },
+          TotalSubscribers: { $first: { $size: "$Subscribers" } },
+          TotalLikes: { $first: { $size: "$Likes" } }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          TotalSubscribers: 1,
+          TotalLikes: 1,
+          TotalVideos: 1,
+          TotalViews: 1
+
+
+        }
+      }
+
+    ])
+
+    if (!channelStat) {
+      throw new apiError(500, "Unable to fetch the channel stat!")
+    }
+    return res
+      .status(200)
+      .json(new apiResponse(200, channelStat[0], "Channel Stat fetched Successfully"))
+  } catch (e) {
+    throw new apiError(500, e?.message || "Unable to fetch the channelm stat!!")
+  }
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
 
+  const userId = req.user?._id;
+  try {
+    const videos = await Video.find({ owner: userId })
+
+    if (!videos || videos.length === 0) {
+      return res
+        .status(200)
+        .json(new apiResponse(200, videos, "No video published yet"))
+    }
+    return res
+      .status(200)
+      .json(new apiResponse(200, videos, "All videos fetched"))
+  } catch (e) {
+    throw new apiError(200, e?.message || "Unable to fetch the videos!!")
+  }
 })
+
 
 export {
   getChannelStats,
